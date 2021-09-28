979b8c60a1ecd20450278a4a8091acdd3f19a8ac
brunodeuner <bruno_deuner@hotmail.com>
2021-09-28 13:08:11 -0300

    Removido métodos da classe variáveis não utilizados Alterado action do sonar para excluir a cobertura dos testes

--- testes/Core/Estudo.Testes.Core.Http/Variáveis/Variáveis.cs
+++ testes/Core/Estudo.Testes.Core.Http/Variáveis/Variáveis.cs
@@ -1,9 +1,5 @@
-﻿using Estudo.Testes.Core.Http.Arquivos;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
 
 namespace Estudo.Testes.Core.Http.Variáveis
 {
@@ -17,33 +13,5 @@ namespace Estudo.Testes.Core.Http.Variáveis
 
         public static void AtribuirValorVariável(string nomeVariável, string valorVariável) =>
             variaveisAtribuicao.Add(nomeVariável, valorVariável);
-
-        public static void AtribuirVariaveis(this object variável, string prefixo = default)
-        {
-            foreach (var propriedade in variável?.GetType()?.GetProperties()?.Where(x => x.CanRead &&
-                !x.GetIndexParameters().Any()))
-            {
-                var valorPropriedade = propriedade.GetValue(variável);
-
-                var nomeVariável = prefixo == default ? propriedade.Name : $"{prefixo}.{propriedade.Name}";
-
-                AtribuirValorVariável(nomeVariável, valorPropriedade?.ToString());
-
-                valorPropriedade.AtribuirVariaveis(nomeVariável);
-            }
-        }
-
-        public static void AtribuirVariaveis<T>() => ObterVariaveis<T>().AtribuirVariaveis();
-
-        public static T ObterVariaveis<T>()
-        {
-            var caminhoJsonVariaveis = Path.Combine(LocalAtualDeExecução.ObterLocalAtualDeExecução(),
-                typeof(T).Name + ".json");
-            using var streamReader = new StreamReader(caminhoJsonVariaveis, Encoding.UTF8);
-            using var reader = new JsonTextReader(streamReader);
-            return jsonSerializer.Deserialize<T>(reader);
-        }
-
-        public static void LimparVariável(string nomeVariável) => variaveisAtribuicao.Remove(nomeVariável);
     }
 }
