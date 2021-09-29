41ec6156945958a2b486ea4a4110179bcced9cbc
brunodeuner <bruno_deuner@hotmail.com>
2021-09-28 21:07:10 -0300

    Criado TestesParaExtensãoDeId

---
+++ testes/Core/Infraestrutura/Armazenamento/Estudo.Infraestrutura.Armazenamento.Abstrações.Testes/TestesParaExtensãoDeId.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace Estudo.Infraestrutura.Armazenamento.Abstrações.Testes
+{
+    public class TestesParaExtensãoDeId
+    {
+        [Fact]
+        public void ObterId_ObjetoSemPropriedadeId_Exceção()
+        {
+            var exceção = Assert.Throws<ArgumentException>(() => new object().ObterId());
+            Assert.Equal("Object não possui propriedade Id que possa ser lida", exceção.Message);
+        }
+
+        [Fact]
+        public void AtribuirNovoId_ObjetoSemPropriedadeId_Exceção()
+        {
+            var exceção = Assert.Throws<ArgumentException>(() => new object().AtribuirNovoId());
+            Assert.Equal("Object não possui propriedade Id que possa ser atribuida", exceção.Message);
+        }
+    }
+}
