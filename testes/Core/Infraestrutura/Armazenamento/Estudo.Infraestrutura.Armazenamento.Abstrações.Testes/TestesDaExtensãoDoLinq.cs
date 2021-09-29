6b274418b03d2807f15f34d73ea2ddd0e60c8e04
brunodeuner <bruno_deuner@hotmail.com>
2021-09-28 21:21:30 -0300

    Criado teste ToAsyncEnumerable_QueryableQueNãoImplementaQueryableDao_Exceção

--- testes/Core/Infraestrutura/Armazenamento/Estudo.Infraestrutura.Armazenamento.Abstrações.Testes/TestesDaExtensãoDoLinq.cs
+++ testes/Core/Infraestrutura/Armazenamento/Estudo.Infraestrutura.Armazenamento.Abstrações.Testes/TestesDaExtensãoDoLinq.cs
@@ -1,4 +1,5 @@
 using Estudo.Infraestrutura.Armazenamento.Abstrações.Queryable;
+using Estudo.Infraestrutura.Armazenamento.Abstrações.Queryable.Leitura;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -26,6 +27,16 @@ namespace Estudo.Infraestrutura.Armazenamento.Abstrações.Testes
             var queryDao = new QueryableDao<object>(query, queryProvider);
             var exceção = await Assert.ThrowsAsync<NotImplementedException>(() => ProcessarRegistros(queryDao));
             Assert.Equal("IDaoProxy não implementa IToAsyncEnumerable", exceção.Message);
+        }
+
+        [Fact]
+        public async Task ToAsyncEnumerable_QueryableQueNãoImplementaQueryableDao_Exceção()
+        {
+            var query = new List<object>().AsQueryable();
+            var queryProvider = new QueryProviderDao(query.Provider, new Mock<IToAsyncEnumerable>().As<IDao>().Object);
+            var queryDao = new QueryableEmbranco<object>(queryProvider);
+            var exceção = await Assert.ThrowsAsync<NotImplementedException>(() => ProcessarRegistros(queryDao));
+            Assert.Equal("QueryableEmbranco`1 não implementa IQueryableDao`1", exceção.Message);
         }
 
         private static async Task ProcessarRegistros(IQueryable<object> query)
