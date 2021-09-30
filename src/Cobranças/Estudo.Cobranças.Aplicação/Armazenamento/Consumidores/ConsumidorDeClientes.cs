9333e4672ddbb8f79d551a32883ba60decc34408
brunodeuner <bruno_deuner@hotmail.com>
2021-09-30 12:59:42 -0300

    #26 Criado evento de pessoa cadastrada e passado a responsabilidade do cadastro para a regra de negócio

--- src/Cobranças/Estudo.Cobranças.Aplicação/Armazenamento/Consumidores/ConsumidorDeClientes.cs
+++ src/Cobranças/Estudo.Cobranças.Aplicação/Armazenamento/Consumidores/ConsumidorDeClientes.cs
@@ -1,8 +1,10 @@
 ﻿using Estudo.Cobranças.Aplicação.Armazenamento.Consumidores.Eventos;
-using Estudo.Cobranças.Domínio.Entidades;
-using Estudo.Cobranças.Domínio.Repositórios;
+using Estudo.Cobranças.Domínio.ManipuladoresDeEventos.Eventos;
+using Estudo.Core.Domínio.Validação;
 using Estudo.Core.Infraestrutura.Armazenamento.Abstrações;
+using Estudo.Core.Infraestrutura.Bus.Abstrações;
 using Estudo.Core.Infraestrutura.Bus.Abstrações.Consumidor;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading;
@@ -27,12 +29,22 @@ namespace Estudo.Cobranças.Aplicação.Armazenamento.Consumidores
             {
                 using var escopo = serviceProvider.CreateScope();
                 var serviceProviderDoEscopo = escopo.ServiceProvider;
-                var repositórioDePessoa = serviceProviderDoEscopo.GetRequiredService<RepositórioDePessoa>();
-                await repositórioDePessoa.Salvar(new Pessoa(requisição.Corpo.Cpf, requisição.Corpo.Estado), cancellationToken);
-                var dao = serviceProviderDoEscopo.GetRequiredService<IDao>();
-                await dao.SalvarAlterações(cancellationToken);
+                await PublicarEventoDePessoadaCadastrada(requisição, serviceProviderDoEscopo, cancellationToken);
+                if (PossuiNotificações(serviceProviderDoEscopo))
+                    return;
+                await SalvarAlterações(serviceProviderDoEscopo, cancellationToken);
             };
             return consumidor.Iniciar(nameof(Cliente), cancellationToken);
         }
+
+        private static Task PublicarEventoDePessoadaCadastrada(EventoEventArgs<Cliente> requisição,
+            IServiceProvider serviços, CancellationToken cancellationToken) => serviços.GetRequiredService<IMediator>()
+            .Publish(new EventoDePessoaCadastrada(requisição.Corpo), cancellationToken);
+
+        private static bool PossuiNotificações(IServiceProvider serviços) =>
+            serviços.GetRequiredService<INotificaçõesDoDomínio>().PossuiNotificações();
+
+        private static ValueTask SalvarAlterações(IServiceProvider serviços, CancellationToken cancellationToken) =>
+            serviços.GetRequiredService<IDao>().SalvarAlterações(cancellationToken);
     }
 }
---
+++ src/Cobranças/Estudo.Cobranças.Domínio/ManipuladoresDeEventos/Eventos/EventoDePessoaCadastrada.cs
@@ -0,0 +1,12 @@
+﻿using Estudo.Cobranças.Domínio.Entidades;
+using Estudo.Core.Domínio.Eventos;
+
+namespace Estudo.Cobranças.Domínio.ManipuladoresDeEventos.Eventos
+{
+    public class EventoDePessoaCadastrada : Evento
+    {
+        public EventoDePessoaCadastrada(Pessoa pessoa) => Pessoa = pessoa;
+
+        public Pessoa Pessoa { get; init; }
+    }
+}
